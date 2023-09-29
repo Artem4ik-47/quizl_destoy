67b7b658aa6840c1054364e0ab20821b967172c4
Artyom <ortem4ik.komya@gmail.com>
2023-09-29 18:13:54 +0300

    ROOT

---
+++ main.py
@@ -0,0 +1,89 @@
+import easyocr
+import pposs
+import time
+from PIL import ImageGrab
+from PIL import Image
+import pyautogui
+rdr = easyocr.Reader(["ru", "en"], gpu = False)
+viv = (0, 0, 0)
+print("Started")
+while viv != (255, 255, 255):
+	scrn = ImageGrab.grab()
+	msc = scrn.load()
+	viv = msc[1130, 1010]
+	#print(viv)
+scrn.save("scrnf.png")
+scrn = Image.open("scrnf.png")
+msc = scrn.load()
+start_time = time.monotonic()
+
+rf = []
+pposs.krt(285, 285 + 460, 380, 420, "1", scrn, msc)#1st LINE
+res = rdr.readtext("t1.png", detail = 0, paragraph = True)
+rf = rf + res
+pposs.krt(785, 785 + 480, 380, 420, "2", scrn, msc)
+res = rdr.readtext("t2.png", detail = 0, paragraph = True)
+rf = rf + res
+pposs.krt(1290, 1290 + 480, 385, 420, "3", scrn, msc)
+res = rdr.readtext("t3.png", detail = 0, paragraph = True)
+rf = rf + res
+pposs.krt(1795, 1795 + 480, 380, 420, "4", scrn, msc)
+res = rdr.readtext("t4.png", detail = 0, paragraph = True)
+rf = rf + res
+
+pposs.krt(285, 285 + 460, 770, 805, "5", scrn, msc)#2nd LINE
+res = rdr.readtext("t5.png", detail = 0, paragraph = True)
+rf = rf + res
+pposs.krt(785, 785 + 480, 770, 805, "6", scrn, msc)
+res = rdr.readtext("t6.png", detail = 0, paragraph = True)
+rf = rf + res
+pposs.krt(1290, 1290 + 480, 770, 805, "7", scrn, msc)
+res = rdr.readtext("t7.png", detail = 0, paragraph = True)
+rf = rf + res
+pposs.krt(1795, 1795 + 480, 770, 805, "8", scrn, msc)
+res = rdr.readtext("t8.png", detail = 0, paragraph = True)
+rf = rf + res
+
+pposs.krt(285, 285 + 460, 1150, 1185, "9", scrn, msc)#3rd LINE
+res = rdr.readtext("t9.png", detail = 0, paragraph = True)
+rf = rf + res
+pposs.krt(785, 785 + 480, 1150, 1185, "10", scrn, msc)
+res = rdr.readtext("t10.png", detail = 0, paragraph = True)
+rf = rf + res
+pposs.krt(1290, 1290 + 480, 1150, 1185, "11", scrn, msc)
+res = rdr.readtext("t11.png", detail = 0, paragraph = True)
+rf = rf + res
+pposs.krt(1795, 1795 + 480, 1150, 1185, "12", scrn, msc)
+res = rdr.readtext("t12.png", detail = 0, paragraph = True)
+rf = rf + res
+
+d = {"белок": "Protein", "газированные наПитки": "Fizzy drinks", "МЯСО ПТИЦЫ": "poultry", "обработанная пища": "processed food", "бобовые": "pulses", "насыщенные жиры, транс жиры": "saturated fats", "цельнозерновые": "wholegrain", "потреблять": "Consume", "питательные вещества": "Nutrients", "питание": "Nutrition", "уменьшать; сокращать": "reduce", "углеводы": "Carbohydrates", "переваривать": "digest", "зарядиться энергией": "boost energy", "разнообразие": "variety", "содержать": "contain", "Клетчатка": "fibre", "избегать": "avoia", "сжигать калории": "burn calories", "оставаться прежним": "remain the same", "оставаться сильным": "stay strong", "Добавки и консерванты": "additives and preservatives", "холестерин": "Cholesterol", "молочные продукты": "dairy products"}
+for an in rf:
+	print(an)
+it = 1
+ans = []
+for vv in rf:
+	if vv in d:
+		it2 = 1
+		for vv2 in rf:
+			if vv2 == d[vv]:
+				ans.append((it, it2))
+			it2 += 1
+	it += 1
+
+dcl = {1: (300, 400), 2: (800, 400), 3: (1400, 400), 4: (1900, 400), 5: (300, 790), 6: (800, 790), 7: (1400, 790), 8: (1900, 790), 9: (300, 1170), 10: (800, 1170), 11: (1400, 1170), 12: (1900, 1170)}
+time.sleep(0.0005)
+for an in ans:
+	pyautogui.moveTo(dcl[an[0]])
+	time.sleep(0.00001)
+	pyautogui.mouseDown(button='left')
+	pyautogui.mouseUp(button='left')
+	pyautogui.moveTo(dcl[an[1]])
+	time.sleep(0.00001)
+	pyautogui.mouseDown(button='left')
+	pyautogui.mouseUp(button='left')
+
+end_time = time.monotonic()
+vivtime = float(end_time - start_time)
+vivtime = round(vivtime, 3)
+print(vivtime)
---
+++ pposs.py
@@ -0,0 +1,48 @@
+from PIL import Image
+def ksf(x, ys, yf, np, msc):
+	xf = x
+	ff = (255, 255, 255)
+	while ff[0] > 230 and ff[1] > 230 and ff[2] > 240:
+		for i in range(ys, yf):
+			ff = msc[xf, i]
+			if ff[0] < 230 and ff[1] < 230 and ff[2] < 240:
+				break
+		if np == "F":
+			xf += 1
+		else:
+			xf -= 1
+	if np == "F":
+		xf -= 2
+	else:
+		xf += 2
+	return xf
+def kud(y, xs, xf, np, msc):
+	yf = y
+	ff = (255, 255, 255)
+	while ff[0] > 230 and ff[1] > 230 and ff[2] > 240:
+		for i in range(xs, xf):
+			ff = msc[i, yf]
+			if ff[0] < 230 and ff[1] < 230 and ff[2] < 240:
+				break
+		if np == "D":
+			yf += 1
+		else:
+			yf -= 1
+	if np == "D":
+		yf -= 2
+	else:
+		yf += 2
+	return yf
+def krt(xs, xf, ys, yf, nmb, scrn, msc):
+	xxs = ksf(xs, ys, yf, "F", msc)
+	xxf = ksf(xf, ys, yf, "R", msc)
+	yys = kud(ys, xxs, xxf, "D", msc)
+	yyf = kud(yf, xxs, xxf, "U", msc)
+	#print(xxs, end = " ")
+	#print(yys, end = "\n")
+	#print(xxf, end = " ")
+	#print(yyf, end = "\n")
+	t = scrn.crop((xxs, yys, xxf, yyf))
+	width, height = t.size
+	#t = t.resize((int(0.7 * width), int(0.7 * height)))
+	t.save("t" + nmb + ".png")
