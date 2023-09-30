303ccfe7adfe2b083ea187d4f39d2f087dcccfbd
Artyom <ortem4ik.komya@gmail.com>
2023-09-30 11:38:30 +0300

    Preferences menu

--- main.py
+++ main.py
@@ -5,12 +5,66 @@ from PIL import Image
 import pyautogui
 import codecs
 viv = (0, 0, 0)
-print("Started")
+
+dela = 0.1
+listc = []
+sccf = open("pref.txt", "r")
+plines = sccf.readlines()
+scc = plines[0]
+lstadd = ""
+for smb in scc:
+	if smb != ",":
+		lstadd = lstadd + smb
+	else:
+		lstadd = int(lstadd)
+		listc.append(lstadd)
+		lstadd = ""
+lstadd = int(lstadd)
+listc.append(lstadd)
+lstadd = ""
+sccf.close()
+debug_mode = ""
+print("Input p to enter preferences: ", end = "")
+pref = input()
+if pref == "p":
+	print("\n__________PREFERENCES__________")
+	print("Input d to enter debug mode: ", end = "")
+	debug_mode = input()
+	if debug_mode == "d":
+		print("You are in debug mode")
+	print("#")
+	print("Input s to manual edit clicks delay (basic: 0.1): ", end = "")
+	m_delay = input()
+	if m_delay == "s":
+		print("Enter delay: ", end = "")
+		dela = float(input())
+	print("#")
+	nw = ""
+	for cco in listc:
+		nw = nw + str(cco) + " "
+	nw = nw[:-1]
+	print("Input r to use manual screenshot box (now: " + nw + "): ", end = "")
+	m_scrbox = input()
+	if m_scrbox == "r":
+		print("Enter screenshot box coordinates: ", end = "")
+		listinp = input()
+		listinp = listinp.split()
+		listc = list(map(int, listinp))
+		sccf = open("pref.txt", "r")
+		plines = sccf.readlines()
+		sccf.close()
+		plines[0] = str(listc[0]) + "," + str(listc[1]) + "," + str(listc[2]) + "," + str(listc[3]) + "\n"
+		sccf = open("pref.txt", "w")
+		for cline in plines:
+			sccf.write(cline)
+		sccf.close()
+	print("_______________________________")
+print("Started\n")
+
 while viv != (255, 255, 255):
-	scrn = ImageGrab.grab(bbox = (285, 320, 2270, 1300))
+	scrn = ImageGrab.grab(bbox = (listc[0], listc[1], listc[2], listc[3]))
 	msc = scrn.load()
 	viv = msc[150, 150]
-	#print(viv)
 scrn.save("scrnf.png")
 start_time = time.monotonic()
 
@@ -34,25 +88,22 @@ dfile.close()
 
 my_conf = r"--psm 4 --oem 1"
 res = pytesseract.image_to_string(scrn, lang = "rus+eng", config = my_conf)
-print(res)
 
 rf = []
 nows = ""
 for now in res:
 	if (now != " " or len(nows) > 0) and now != "\n":
 		nows += now
-	print(nows)
+	if debug_mode == "d":
+		print(nows)
 	if nows in ltd:
 		rf.append(nows)
 		nows = ""
 
-end_time = time.monotonic()
-vivtime = float(end_time - start_time)
-vivtime = round(vivtime, 3)
-print(vivtime)
-
+print("==========Recognised words===========\n")
 for an in rf:
 	print(an)
+print("\n=================End=================\n")
 it = 1
 ans = []
 for vv in rf:
@@ -68,6 +119,6 @@ dcl = {1: (300, 400), 2: (800, 400), 3: (1400, 400), 4: (1900, 400), 5: (300, 79
 time.sleep(0.005)
 for an in ans:
 	pyautogui.click(dcl[an[0]])
-	time.sleep(0.075)
+	time.sleep(dela)
 	pyautogui.click(dcl[an[1]])
-	time.sleep(0.075)
+	time.sleep(dela)
